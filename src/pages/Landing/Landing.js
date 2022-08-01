4393d876d9e56031215b019a3a8569b46f727a91
alicialowe <hohpascal@outlook.com>
2022-08-01 17:59:54 +0100

    linked button to About page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
 import ParticlesBackground from "./ParticlesBackground.js";
 
 import startButton from "../../assets/start.webp";
@@ -21,7 +22,7 @@ function Landing() {
             <ParticlesBackground />
             <h1>Alicia Lowe<br></br>totally not sus - <mark className="red">promise</mark></h1>
 
-            {showButton && <button className="start-button"><img className="start-button-img" src={startButton} /></button>}
+            {showButton && <button className="start-button"><Link to="/about"><img className="start-button-img" src={startButton} /></Link></button>}
         </>
     )
 
