86300d728ca404f245705b945de842cc522ea2ca
alicialowe <hohpascal@outlook.com>
2022-08-01 17:45:09 +0100

    button image now black background as unable to make transparent for some reason. set to come on with timer but screen still glitches

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -4,23 +4,24 @@ import ParticlesBackground from "./ParticlesBackground.js";
 
 import startButton from "../../assets/start.webp";
 
-// import "../../App.css";
+import "../../App.css";
 
 function Landing() {
-    const [enter, setEnter] = useState("");
+    const [showButton, setShowButton] = useState(false);
     
-        // setTimeout(() => {
-        //     setEnter(<button><img src={startButton} /></button>);
-        // }, 7000);
 
-       
+        useEffect(() => {
+            setInterval(() => {
+                setShowButton(!showButton)    
+            }, 7000)}, []);
     
 
     return (
         <>
             <ParticlesBackground />
             <h1>Alicia Lowe<br></br>totally not sus - <mark className="red">promise</mark></h1>
-            {/* <>{enter}</> */}
+
+            {showButton && <button className="start-button"><img className="start-button-img" src={startButton} /></button>}
         </>
     )
 
